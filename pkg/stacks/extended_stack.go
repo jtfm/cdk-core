42d54f4618e61ad2bfc63fd179b570e289174e16
Jack Milner <jackmilner@gmail.com>
2023-02-26 21:38:06 +0000

    Added extended stack

--- pkg/stacks/extended_stack.go
+++ pkg/stacks/extended_stack.go
@@ -2,6 +2,7 @@ package stacks
 
 import (
 	"fmt"
+
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
@@ -12,18 +13,26 @@ import (
 type ExtendedStackProps struct {
 	awscdk.StackProps
 	Version *string
-	Build *string
+	Build   *string
+}
+
+type ExtendedStack struct {
+	awscdk.Stack
 }
 
 func NewExtendedStack(
-	scope constructs.Construct, 
-	id *string, 
-	props *ExtendedStackProps) awscdk.Stack {
+	scope constructs.Construct,
+	id *string,
+	props *ExtendedStackProps) ExtendedStack {
 
 	props.StackName = jsii.String(fmt.Sprintf(
 		"%s-%s-%s", *props.StackName, *props.Version, *props.Build))
 
 	stack := awscdk.NewStack(scope, id, &props.StackProps)
 
-	return stack
+	return ExtendedStack{stack}
+}
+
+func (s ExtendedStack) GetName() *string {
+	return s.StackName()
 }
