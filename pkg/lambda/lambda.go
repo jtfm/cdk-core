3ec930d829f99c47b97a42675c17999b4883b4a3
Jack Milner <jack.milner@bigupdata.co.uk>
2023-03-16 21:48:25 +0000

    Added port parameter to SwitchingListenAndServ and portvalidation

--- pkg/lambda/lambda.go
+++ pkg/lambda/lambda.go
@@ -1,21 +1,39 @@
 package lambda
 
 import (
-	"os"
 	"net/http"
+	"os"
+	"strconv"
+
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 // A function that runs a router in a lambda environment or a local server
 // depending on whether the function is running in a lambda environment or not.
-func SwitchingListenAndServe(router func() http.Handler) {
-	if isInLambda() {
+// The port argument is ignored if the function is running in a lambda environment.
+func SwitchingListenAndServe(router func() http.Handler, port string) {
+	if IsInLambda() {
 		lambda.Start(router())
 	} else {
-		http.ListenAndServe(":8080", router())
+		validatePort(port)
+		http.ListenAndServe(port, router())
 	}
 }
 
-func isInLambda() bool {
+// Returns true if the function is running in a lambda environment.
+func IsInLambda() bool {
 	return os.Getenv("AWS_LAMBDA_FUNCTION_NAME") != ""
+}
+
+func validatePort(port string) {
+	if port[0:1] != ":" {
+		panic("Port must start with a colon")
+	}
+	portInt, err := strconv.Atoi(port[1:])
+	if err != nil {
+		panic("Port must be a number")
+	}
+	if portInt < 1 || portInt > 65535 {
+		panic("Port must be between 1 and 65535")
+	}
 }
